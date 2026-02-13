cedc4419c91c20e956e48001b60cbecc27c1f9ef
Rafael Julio <rafael.julio.dev@outlook.com>
2026-02-13 17:14:00 -0300

    Feat - Ethereum config
    - Started hardhat folder for contract building
    - Finished sepolia test network config on web application

--- hooks/use-connect-wallet.tsx
+++ hooks/use-connect-wallet.tsx
@@ -9,13 +9,49 @@ export function useConnectWallet() {
   const [address, setAddress] = useState<string>();
   const [connected, setConnected] = useState(false);
 
+  const rpcUrl = process.env.NEXT_PUBLIC_RPC_URL!;
+
   const connectWallet = async () => {
     try {
       if (!window.ethereum) {
         alert("Instale o Meta Mask!!");
         return
       }
+
+
       await window.ethereum.request({ method: "eth_requestAccounts" });
+
+      try {
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: "0xAA36A7" }], // Chain ID Sepolia em hex
+        });
+      } catch (e: any) {
+        console.log('[ERRO], BUSCANDO CONFIGURAÇÃO DA REDE SEPOLIA');
+        if (e.code === 4902) {
+          await window.ethereum.request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: "0xAA36A7",
+                chainName: "Sepolia Test Network",
+                rpcUrls: [
+                  rpcUrl,
+                ],
+                nativeCurrency: {
+                  name: "SepoliaETH",
+                  symbol: "SEP",
+                  decimals: 18,
+                },
+                blockExplorerUrls: ["https://sepolia.etherscan.io/"],
+              },
+            ],
+          });
+        }
+
+      }
+
+
       const p = new BrowserProvider(window.ethereum);
       const s = await p.getSigner();
       const a = await s.getAddress();
